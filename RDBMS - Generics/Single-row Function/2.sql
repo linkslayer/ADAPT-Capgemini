53d4f0a379e4bfd098c756d312e0a0d029cdd583
Kshitij Singh <80029785+linkslayer@users.noreply.github.com>
2021-03-19 11:36:28 +0530

    Create 2.sql

---
+++ RDBMS - Generics/Single-row Function/2.sql
@@ -0,0 +1,6 @@
+alter session set current_schema=bank;
+SET heading OFF;
+
+SELECT TRANS_DT, SUBSTR(TRANS_DESC,1,20) AS TRANS_DESC, TRANS_AMT
+FROM BANK_TRANSACTION
+WHERE TO_CHAR(TRANS_DT, 'Mon-YYYY') = 'Jun-2010';
