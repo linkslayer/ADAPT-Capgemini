4209bfd259205bd36677a0fccee9bb2b60df2343
Kshitij Singh <80029785+linkslayer@users.noreply.github.com>
2021-04-25 18:35:54 +0530

    Create Coding Java : Loop 1.java

---
+++ Programming Using Java - Stage 2 IND/Coding Java : Loop 1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Main class
+ */
+public class Source {
+    /**
+    * Main method
+    * @param args 
+    */
+    public static void main(String[] args) {
+	
+		// Student Code Begin
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		if (num>=1 && num <=999){
+		    int[] bin = new int[40];
+		    int index=0;
+		    while(num>0){
+		        bin[index] = num % 2;
+		        num /= 2;
+            index++;
+		    }
+		    for(int i=index-1;i>=0;i--){
+		        System.out.print(bin[i]);
+		    } 
+		}
+		else{
+		    System.out.println("INVALID_INPUT");
+		}
+		// Student Code end
+    }
+}
