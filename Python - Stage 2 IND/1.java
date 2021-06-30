a357ea9d328fc87986205729b991f849232fcf62
Kshitij Singh <80029785+linkslayer@users.noreply.github.com>
2021-06-30 18:24:23 +0530

    Update 1.java

--- Python - Stage 2 IND/1.java
+++ Python - Stage 2 IND/1.java
@@ -1,118 +1,90 @@
-import java.util.*;
 
-public class Main{
-	static Scanner in = new Scanner(System.in);
-	static void driver(int c) {
-		switch(c) {
-		case 1:
-			addition();
-			break;
-		case 2:
-			subtraction();
-			break;
-		case 3:
-			mult();
-			break;
-		case 4:
-			div();
-			break;
-		case 5:
-			modulo();
-			break;
-		case 6:
-			power();
-			break;
-		case 7:
-			square();
-			break;
-		case 8:
-			square_root();
-			break;
-		case 9:
-			System.exit(0);
-		default:
-			System.out.println("Incorrect Choice, Enter from menu");
-			break;
+import java.util.Scanner;
+
+public class Main {
+	static int randGen(int n) {
+		n = 1 + (int)(n* Math.random());
+		return n;
 	}
+	static int diff(int r,int guess) {
+		if(r>guess)
+			return r-guess;
+		else
+			return guess-r;
 	}
-	
-	static void printer() {
-		System.out.println("\nBASIC CALCULATOR: \n");
-		System.out.print("1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION\n5.MODULO\n6.POWER\n7.SQAURE\n8.SQUARE ROOT\n9.EXIT\n\nENTER YOUR CHOICE:\n");
-	}
-	
-	static void print_num_statement() {
-		System.out.println("Enter Two Numbers:");
-	}
-	
-	static int[] getInput() {
-		int[] n = new int[2];
-        n[0] = in.nextInt();
-        n[1] = in.nextInt();
-        return n;
-	}
-	
-	static void addition() {
-		print_num_statement();
-		int[] n = getInput();
-		System.out.println("\nADDITION: "+n[0]+"+"+n[1]+"="+(n[0]+n[1]));
+	static int menu(int score) {
+		System.out.println("NUMBER GUESSING GAME\n");
+		System.out.println("Score: "+score);
+		System.out.println("\nChoose Range-\n1. 1-10\n2. 1-100\nEnter your choice:");
+		int ch = in.nextInt();
+		int max;
+		if (ch==1) {
+			max = 10;
+		} else {
+			max=100;
+		}
+		return max;
 		
 	}
-	
-	static void subtraction() {
-		print_num_statement();
-		int[] n = getInput();
-		System.out.println("\nSUBTRACTION: "+n[0]+"-"+n[1]+"="+(n[0]-n[1]));
-		
-	}
-	
-	static void mult() {
-		print_num_statement();
-		int[] n = getInput();
-		System.out.println("\nMULTIPLICATION: "+n[0]+"*"+n[1]+"="+(n[0]*n[1]));
-		
-	}
-	
-	static void div() {
-		print_num_statement();
-		int[] n = getInput();
-		System.out.println("\nDIVISION: "+n[0]+"/"+n[1]+"="+(n[0]/n[1]));
-		
-	}
-	
-	static void modulo() {
-		print_num_statement();
-		int[] n = getInput();
-		System.out.println("\nMODULO: "+n[0]+"%"+n[1]+"="+(n[0]%n[1]));
-	}
-	
-	static void power() {
-		System.out.println("Enter Number and Power\n");
-		int[] n = getInput();
-		System.out.println("\nPOWER: "+n[0]+"^"+n[1]+"="+(Math.pow(n[0], n[1])));
-	}
-	
-	static void square() {
-		System.out.println("\nEnter a number: ");
-		int n = in.nextInt();
-		System.out.println("\nSQUARE: "+n+"*"+n+"="+(n*n));
-	}
-	
-	static void square_root() {
-		System.out.println("\nEnter a number: ");
-		int n = in.nextInt();
-		System.out.println("\nSQUARE ROOT: "+n+"="+(Math.sqrt(n)));
-	}
-	
-	public static void main(String[] args) {
-		int c;
-		char ch;
+	static int checkActualNumber() {
+		int guess;
 		do {
-			printer();
-			c = in.nextInt();
-			driver(c);
-			System.out.println("\nUse Again? YES(Y)/NO(N)");
-			ch = in.next().charAt(0);
-		}while(ch!='N' || ch!='n');
+			System.out.println("Enter Guess input: ");
+			guess = in.nextInt();
+		}while(guess!=(int)guess);
+		return guess;
 	}
+	static boolean checker(int r, int guess) {
+		if(r==guess)
+			return true;
+		else
+			return false;
+		
+	}
+	static void hints(int r, int guess) {
+		System.out.println("HINT 1: The diffrence between the Random and Guess number: "+guess+" is: "+diff(r, guess));
+		if(r>guess)
+			System.out.println("HINT 2: The random number is greater than " + guess);
+		else
+			System.out.println("HINT 2: The random number is less than " + guess);
+		if(r%2==0) 
+			System.out.println("HINT 3: The random number is even");
+		else
+			System.out.println("HINT 3: The random number is odd");
+	}
+	static Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
+		char choice;
+		int score = 5;
+		do {
+			int max=menu(score);
+			int r = randGen(max);
+			System.out.println("\nRANDOM NUMBER GENERATED\n");
+			boolean condition=true;
+			do {
+				int guess = checkActualNumber();
+				if(checker(r, guess)) {
+					System.out.println("Correct!!! You WON!!!\nThe number was: "+guess);
+					System.out.println("Score will be increased by 5 points");
+					score += 5;
+					condition=false;
+				}else {
+					char h;
+					System.out.println("OOPS!!! Wrong Number\nNeed hints?\nThis will decrease your score by 2\nYes(Y)/No(N)");
+					h=in.next().charAt(0);
+					if(Character.toUpperCase(h)=='Y'){
+						score -= 2;
+						hints(r, guess);
+					}
+					else {
+						System.out.println("Try Again!\n");
+					}
+				}
+			}while(condition);
+			System.out.println("\nPlay Again?YES(Y)/NO(N)");
+			choice = in.next().charAt(0);
+		}while(Character.toUpperCase(choice)!='N');
+		
+	}
+
 }
